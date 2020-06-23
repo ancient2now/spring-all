a06fe2fd11016c6f19f51c0ab36fb0258ceaa944
Li Junqiu <ancient2now@163.com>
2020-06-23 14:03:25 +0800

    test: add FoodFactory

---
+++ spring-boot/src/main/java/com/akikun/springboot/test/FoodFactory.java
@@ -0,0 +1,73 @@
+package com.akikun.springboot.test;
+
+import java.util.Scanner;
+
+public class FoodFactory {
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int lineNums = scan.nextInt();
+        while (lineNums-- > 0) {
+            String cuisineKey = scan.next();
+            String dish = scan.next();
+            Cuisine cuisine = FoodFactory.getFactory().serveFood(cuisineKey, dish);
+
+            if (cuisine != null) {
+                String print = String.format("Serving %s (%s)", cuisine.getDish(), cuisine.name());
+                System.err.println(print);
+            }
+        }
+    }
+
+    private static FoodFactory instance = null;
+
+    public static FoodFactory getFactory() {
+        if (instance == null) {
+            synchronized (FoodFactory.class) {
+                if (instance == null) {
+                    instance = new FoodFactory();
+                }
+            }
+        }
+        return instance;
+    }
+
+    Cuisine serveFood(String cuisineKey, String dish) {
+        try {
+            Cuisine cuisine = Cuisine.valueOf(cuisineKey);
+            cuisine.serveFood(dish);
+            return cuisine;
+        } catch (Exception e) {
+            String print = String.format("Unservelable cuisine %s for dish %s ", cuisineKey, dish);
+            System.err.println(print);
+            return null;
+        }
+    }
+
+    enum Cuisine {
+
+        Chinese,
+
+        Italian,
+
+        Japanese,
+
+        Mexican;
+
+        private final String[] dish = new String[]{""};
+
+        void serveFood(String dish) {
+            this.dish[0] = dish;
+        }
+
+        String getDish() {
+            return dish[0];
+        }
+
+    }
+}
+
+
+
+
+
